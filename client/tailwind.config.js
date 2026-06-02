aee28f63c80fa16e879e16784f0f0749441376e8
jpravin664 <manopravin1@gmail.com>
2026-06-02 13:04:20 +0530

    Added font size configurations to tailwind.config.js for improved typography

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -20,6 +20,16 @@ module.exports = {
           muted: '#8b949e',
           text: '#e6edf3',
         }
+      },
+      // 👇 Add this block to increase font sizes
+      fontSize: {
+        'xs': ['0.995rem', { lineHeight: '1.25rem' }],   // was 0.75rem
+        'sm': ['1.1rem', { lineHeight: '1.5rem' }],        // was 0.875rem
+        'base': ['1.125rem', { lineHeight: '1.75rem' }], // was 1rem
+        'lg': ['1.35rem', { lineHeight: '1.75rem' }],    // was 1.125rem
+        'xl': ['1.6rem', { lineHeight: '2rem' }],        // was 1.25rem
+        '2xl': ['1.975rem', { lineHeight: '2.25rem' }],  // was 1.5rem
+        // You can also scale larger sizes similarly
       }
     }
   },
